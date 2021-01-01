d1816d980e4991f57bdd175e86b616d7518b1d92
newmanne <newman.n8@gmail.com>
2021-01-01 12:08:53 -0500

    add statics

---
+++ src/components/SectionSeparator.vue
@@ -0,0 +1,13 @@
+<template>
+    <q-separator class="q-my-xl q-py-xs" color="#888888"/>
+</template>
+
+<script>
+  export default {
+    name: 'SectionSeparator'
+  }
+</script>
+
+<style scoped>
+
+</style>
